6da1fd76b75042819c908cb62177ca80bd10a00e
Loris Mendolia <lorismendolia@hotmail.com>
2024-06-25 13:25:59 +0200

    Added license info

--- include/AD5674.h
+++ include/AD5674.h
@@ -1,3 +1,25 @@
+/*
+Copyright (c) 2024 Loris Mendolia
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #ifndef AD5674_h
 #define AD5674_h
 
@@ -44,4 +66,14 @@ class AD5674Class{
 		void writeData(byte command, byte address, word data);
 };
 
-#endif 
+#endif
+
+/*
+To-do:
+- Add support for daisy-chaining
+- Add support for readback
+- Add support for LDAC mask register
+- Add support for writing to all input registers
+- Add support for writing to all DAC registers
+- Add support for software reset
+*/
